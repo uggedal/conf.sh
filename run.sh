9d62cee4d7ae591c3c3601212429c1c72c4e9979
Eivind Uggedal <eivind@uggedal.com>
2013-11-17 14:08:34 +0100

    s/environment/env

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 [ $# -eq 2 ] || {
-  printf '%s: <host> <environment>\n' $0
+  printf '%s: <host> <env>\n' $0
   exit 64
 }
 
 user=${user:-root}
 host=$1
-environment=$(readlink -f $2)
+env=$(readlink -f $2)
 repo=$(basename $(dirname $(readlink -f $0)))
 
 roles() {
-  . $environment
+  . $env
   for role in $roles; do
     printf '&& ./roles/%s.sh' $role
   done
@@ -22,4 +22,4 @@ roles() {
   tar cpf - $repo | ssh $user@$host "tar xpf - -C /tmp"
 )
 
-ssh $user@$host "$(cat $environment) && cd /tmp/$repo $(roles)"
+ssh $user@$host "$(cat $env) && cd /tmp/$repo $(roles)"
