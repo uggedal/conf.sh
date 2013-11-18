16a134e572afcae824071cc5cb279ff343c1192c
Eivind Uggedal <eivind@uggedal.com>
2013-11-18 11:09:18 +0100

    host in env file

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-[ $# -eq 2 ] || {
-  printf '%s: <host> <env>\n' $0
+[ $# -eq 1 ] || {
+  printf '%s: <env>\n' $0
+  exit 64
+}
+
+env=$(readlink -f $1)
+
+. $env
+
+[ -n "$host" ] || {
+  printf 'no host in %s\n' $env
   exit 64
 }
 
 user=${user:-root}
-host=$1
-env=$(readlink -f $2)
 repo=$(basename $(dirname $(readlink -f $0)))
 dest=/tmp/$repo
 
 roles() {
-  . $env
   for role in $roles; do
     printf ' && %s/roles/%s.sh' $dest $role
   done
