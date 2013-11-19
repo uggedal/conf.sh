63374b73a2a909a4d7f2834e2bdf308554364aeb
Eivind Uggedal <eivind@uggedal.com>
2013-11-19 14:11:10 +0100

    user: dotfiles

--- roles/user.sh
+++ roles/user.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-id $_user_name >/dev/null 2>&1 || adduser -D -s $_user_shell -G $_user_group $_user_name
+home_dir=/home/$_user_name
+ssh_dir=$home_dir/.ssh
+ssh_auth_file=$ssh_dir/authorized_keys
+
+id $_user_name >/dev/null 2>&1 \
+  || adduser -D -s $_user_shell -G $_user_group $_user_name
 for g in $_user_groups; do
   id -nG $_user_name | grep -q $g || addgroup $_user_name $g
 done
 
 grep ^$_user_name:!: /etc/shadow >/dev/null && passwd -u $_user_name
 
-ssh_dir=/home/$_user_name/.ssh
-ssh_auth_file=$ssh_dir/authorized_keys
 inode dir $ssh_dir 700 $_user_name $_user_group
 inode file $ssh_auth_file 600 $_user_name $_user_group
 
-if ! fgrep "$_user_authorized_key" $ssh_auth_file >/dev/null; then
+fgrep "$_user_authorized_key" $ssh_auth_file >/dev/null || \
   printf '%s\n' "$_user_authorized_key" >> $ssh_auth_file
-fi
+
+[ -d $home_dir/.git ] || \
+  sudo -u $_user_name sh -c "cd $home_dir && git init"
+
+inode dir $home_dir/.git 755 $_user_name $_user_group
+inode file $home_dir/.git/config 644 $_user_name $_user_group
+tmpl dotfiles_config $home_dir/.git/config
