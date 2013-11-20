5b56cbd818abb4eab8bf399edc8cd483e18ab586
Eivind Uggedal <eivind@uggedal.com>
2013-11-20 09:36:12 +0100

    sshd: use notify for sshd restart

--- roles/sshd.sh
+++ roles/sshd.sh
@@ -4,11 +4,6 @@ set -e
 
 pkg openssh
 
-# TODO: this is ugly:
-set +e
-tmpl sshd_config /etc/ssh/sshd_config
-set -e
+tmpl sshd_config /etc/ssh/sshd_config sshd
 
-if  [ $? -eq 100 ]; then
-  daemon restart sshd
-fi
+[ "$state" = changed ] && daemon restart sshd
