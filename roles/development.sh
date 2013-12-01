3fc2e3f222f582c598a8f618ab21df299b3b771c
Eivind Uggedal <eivind@uggedal.com>
2013-12-01 14:02:21 +0000

    development: bash completion

--- roles/development.sh
+++ roles/development.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-pkg bash vim tmux git hub mercurial less
+pkg bash bash-completion@testing vim less
+pkg tmux tmux-bash-completion
+pkg git git-bash-completion hub mercurial
 pkg htop lsof
