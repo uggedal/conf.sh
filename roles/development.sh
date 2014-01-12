07cc39af055cccb7ee335059504459a89e3d8d9c
Eivind Uggedal <eivind@uggedal.com>
2014-01-12 16:29:10 +0000

    development: hub

--- roles/development.sh
+++ roles/development.sh
@@ -1,8 +1,11 @@
 development_role() {
   pkg use dev-vcs/git -gpg -webdav
+  pkg use dev-lang/ruby -rdoc -yaml
+  pkg accept =dev-vcs/hub-1.11.0 ~amd64
   pkg world \
     app-editors/vim \
     app-misc/tmux \
     dev-vcs/git \
+    dev-vcs/hub \
     app-shells/bash-completion
 }
