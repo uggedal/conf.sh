212d816494967e7b58c6bd0bba38def4f1d2e163
Eivind Uggedal <eivind@uggedal.com>
2014-01-30 20:53:23 +0000

    shell: dash as /bin/sh

--- roles/shell.sh
+++ roles/shell.sh
@@ -1,3 +1,9 @@
 shell_role() {
+  pkg world app-shells/dash
+
+  [ -x /bin/dash ] && \
+    [ /bin/dash != "$(readlink -f /bin/sh)" ] && \
+    ln -sf /bin/dash /bin/sh
+
   tmpl inputrc /etc/inputrc
 }
