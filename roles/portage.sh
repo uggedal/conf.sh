87f036328a7e7155a5d4b0f945016c4562579778
Eivind Uggedal <eivind@uggedal.com>
2014-02-02 11:28:39 +0000

    portage: create new DISTDIR and PKGDIR dirs

--- roles/portage.sh
+++ roles/portage.sh
@@ -1,4 +1,9 @@
 portage_role() {
   tmpl rsync_excludes /etc/portage/rsync_excludes && \
-    tmpl make.conf /etc/portage/make.conf
+    tmpl make.conf /etc/portage/make.conf || return 1
+
+  . /etc/portage/make.conf
+  for dir in $DISTDIR $PKGDIR; do
+    inode dir $dir 0775 root portage || return 1
+  done
 }
