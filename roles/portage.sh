377c24b2fcc5b6af53cf5ddb82c62c8004246a64
Eivind Uggedal <eivind@uggedal.com>
2014-02-02 18:09:39 +0000

    portage: fix dir perms

--- roles/portage.sh
+++ roles/portage.sh
@@ -3,7 +3,6 @@ portage_role() {
     tmpl make.conf /etc/portage/make.conf || return 1
 
   . /etc/portage/make.conf
-  for dir in $DISTDIR $PKGDIR; do
-    inode dir $dir 0775 root portage || return 1
-  done
+  inode dir $DISTDIR 775 root portage
+  inode dir $PKGDIR 2775 root portage
 }
