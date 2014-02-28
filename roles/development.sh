8f7a5ed42825579924f4e1332075a285df65f58c
Eivind Uggedal <eivind@uggedal.com>
2014-02-28 20:35:59 +0000

    development: ghi 0.9.2

--- roles/development.sh
+++ roles/development.sh
@@ -1,6 +1,6 @@
 development_role() {
   pkg accept =dev-vcs/hub-1.11.1 ~amd64
-  pkg accept =dev-util/ghi-0.9.1 ~amd64
+  pkg accept =dev-util/ghi-0.9.2 ~amd64
   pkg accept =dev-util/clib-9999 ~amd64
   pkg accept =dev-util/cloc-1.60 ~amd64
 
