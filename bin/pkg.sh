720a14eaa9f7c706199d205fdfd1fc377bb46d0e
Eivind Uggedal <eivind@uggedal.com>
2014-03-16 11:28:28 +0000

    pkg: abstract progress

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,41 +1,19 @@
-# TODO: abstract progress
-
 _pkg_add() {
   local err rc
 
   for p do
-    apk info --quiet --installed $p || {
-      progress start 'pkg add' $p
-      err=$(apk add --quiet $p 2>&1)
-      rc=$?
-      progress finish $rc
-      [ -z "$err" ] || progress result "$err"
-      [ $rc -eq 0 ] || return $rc
-    }
+    apk info --quiet --installed $p || \
+      progress wrap 'pkg add' $p "apk add --quiet $p"
   done
 }
 
 _pkg_sync() {
-  local err rc
-
-  progress start pkg sync
-  err=$(apk update --quiet 2>&1)
-  rc=$?
-  progress finish $rc
-  [ -z "$err" ] || progress result "$err"
-  [ $rc -eq 0 ] || return $rc
+  progress wrap pkg sync 'apk update --quiet'
 }
 
 # TODO: apk update returns 0 on wget error
 _pkg_upgrade() {
-  local err rc
-
-  progress start pkg upgrade
-  err=$(apk upgrade --quiet 2>&1)
-  rc=$?
-  progress finish $rc
-  [ -z "$err" ] || progress result "$err"
-  [ $rc -eq 0 ] || return $rc
+  progress wrap pkg upgrade 'apk upgrade --quiet'
 }
 
 pkg() {
