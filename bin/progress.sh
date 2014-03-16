7d260bff9cb6f285a7fcb5c9cc735c3b4b655dc5
Eivind Uggedal <eivind@uggedal.com>
2014-03-16 11:28:13 +0000

    progress: wrapper

--- bin/progress.sh
+++ bin/progress.sh
@@ -18,6 +18,20 @@ _progress_result() {
   printf '%s\n' "$1" | sed 's/^/  /'
 }
 
+_progress_wrap() {
+  local action="$1"
+  local object="$2"
+  local cmd="$3"
+  local err rc
+
+  progress start "$action" "$object"
+  err=$(eval "$cmd" 2>&1)
+  rc=$?
+  progress finish $rc
+  [ -z "$err" ] || progress result "$err"
+  [ $rc -eq 0 ] || return $rc
+}
+
 progress() {
   action=_progress_$1
   shift
