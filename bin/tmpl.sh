49cbd9aece3f9f0aaac096019803491650086211
Eivind Uggedal <eivind@uggedal.com>
2014-03-21 09:04:42 +0000

    tmpl: support sections

--- bin/tmpl.sh
+++ bin/tmpl.sh
@@ -6,18 +6,53 @@ tmpl() {
   local diff rc
 
   local tmpl_awk='
-{
-  print substitute($0)
+BEGIN {
+  section_ignore = 0
+  buf = ""
 }
 
-function substitute(raw) {
-  if (match(raw, /\{\{([^}]*)\}\}/)) {
-    tag = substr(raw, RSTART, RLENGTH)
-    key = substr(raw, RSTART+2, RLENGTH-4)
-    gsub(tag, ENVIRON[key], raw)
-    return substitute(raw)
-  } else {
-    return raw
+{
+  buf = ""
+  parse($0)
+  if (!(section_ignore && length(buf) == 0)) {
+    print buf
+  }
+}
+
+function push(chunk) {
+  if (!section_ignore) {
+    buf = (buf chunk)
+  }
+}
+
+function parse(line) {
+  cursor = 1
+
+  while (1) {
+    unprocessed = substr(line, cursor)
+    has_match = match(unprocessed, /\{\{[^}]*\}\}/)
+
+    if (!has_match) {
+      push(unprocessed)
+      return
+    }
+
+    cursor = cursor + RSTART + RLENGTH - 1
+    push(substr(unprocessed, 0, RSTART - 1))
+
+    modifier = substr(unprocessed, RSTART+2, 1)
+
+    if (modifier == "#") {
+      key = substr(unprocessed, RSTART+3, RLENGTH-5)
+      if (length(ENVIRON[key]) == 0) {
+        section_ignore = 1
+      }
+    } else if (modifier == "/") {
+      section_ignore = 0
+    } else {
+      key = substr(unprocessed, RSTART+2, RLENGTH-4)
+      push(ENVIRON[key])
+    }
   }
 }
 '
