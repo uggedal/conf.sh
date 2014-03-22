f6da9daf83219c327b9610e8f491e7186c567da5
Eivind Uggedal <eivind@uggedal.com>
2014-03-22 10:34:10 +0000

    network: fix typo in bridge detection

--- roles/network.sh
+++ roles/network.sh
@@ -1,5 +1,5 @@
 network_role() {
-  [ -z "$_network_brigde_if" ] || pkg add bridge || return 1
+  [ -z "$_network_bridge_if" ] || pkg add bridge || return 1
 
   tmpl network.interfaces /etc/network/interfaces
 }
