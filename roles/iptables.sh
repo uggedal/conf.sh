fdfc07ee3a690af26a9ecc75ca2eb899a1b4cb18
Eivind Uggedal <eivind@uggedal.com>
2014-03-24 08:57:34 +0000

    iptables: config before start

--- roles/iptables.sh
+++ roles/iptables.sh
@@ -1,6 +1,6 @@
 iptables_role() {
   pkg add iptables && \
+    tmpl iptables.rule-save /etc/iptables/rules-save iptables && \
     daemon enable iptables && \
-    daemon start iptables && \
-    tmpl iptables.rule-save /etc/iptables/rules-save iptables
+    daemon start iptables
 }
