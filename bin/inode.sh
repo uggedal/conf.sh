a6f38c7e1501bad6fc5eb59321dbf5c8d2114c28
Eivind Uggedal <eivind@uggedal.com>
2014-03-26 11:27:46 +0000

    inode: link support

--- bin/inode.sh
+++ bin/inode.sh
@@ -1,41 +1,39 @@
 inode() {
   local type=$1
-  local p=$2
-  local mode=$3
-  local user=$4
-  local group
-  local rc=0
-  local s_mode s_user s_group
+  local node=$2
+  shift 2
 
-  if [ $# -eq 5 ]; then
-    group=$5
-  else
-    group=$(id -ng $user)
-  fi
+  [ $type = link ] && {
+    local target=$1
+    shift
+  }
+
+  local mode=$1
+  local user=$2
+  local group=$3
+
+  [ -n "$user" ] || user=root
+  [ -n "$group" ] || group=$(id -ng $user)
 
   case $type in
     dir)
-      [ -d $p ] || {
-        progress wrap "$type create" $p "mkdir -p $p"
-        rc=$?
-      }
+      [ -d $node ] || \
+        progress wrap "$type create" $node "mkdir -p $node" || return 1
       ;;
     file)
-      [ -f $p ] || {
-        progress wrap "$type create" $p "touch $p"
-        rc=$?
-      }
+      [ -f $node ] || \
+        progress wrap "$type create" $node "touch $node" || return 1
+      ;;
+    link)
+      [ -L $target ] || \
+        progress wrap "$type create" $node "ln -s $node $target" || return 1
       ;;
   esac
 
-  [ $rc -eq 0 ] || return $rc
-
-  s_mode=$(stat -c %a $p)
-  s_user=$(stat -c %U $p)
-  s_group=$(stat -c %G $p)
-
-  [ $s_mode = $mode -a $s_user = $user -a $s_group = $group ] && return 0
-
-  progress wrap "$type mode" $p \
-    "chmod $mode $p && chown $user $p && chgrp $group $p"
+  [ $(stat -c %a $node) = $mode ] || \
+    progress wrap "$type mode" $node "chmod $mode $node"
+  [ $(stat -c %U $node) = $user ] || \
+    progress wrap "$type user" $node "chown $user $node"
+  [ $(stat -c %G $node) = $group ] || \
+    progress wrap "$type group" $node "chgrp $group $node" 
 }
