7087b72a38f6ad1c83f01edfd4003e6142f6893b
Eivind Uggedal <eivind@uggedal.com>
2014-03-26 09:14:56 +0000

    lxc: create

--- roles/lxc.sh
+++ roles/lxc.sh
@@ -1,4 +1,22 @@
+_lxc_create() {
+  local container=$1
+  local name=${container%%:*}
+  local release=${container##*:}
+  local cmd
+
+  lxc-ls | grep -q ^$name\$ && return
+
+  cmd="lxc-create -n $name -B $_lxc_fs -t alpine"
+  cmd="$cmd -- --repository $_lxc_mirror/$release/main"
+
+  progress wrap lxc $name "$cmd >/dev/null"
+}
+
 lxc_role() {
   pkg add lxc lxc-templates && \
     tmpl lxc.default.conf /etc/lxc/default.conf
+
+  for container in $_lxc_containers; do
+    _lxc_create $container
+  done
 }
