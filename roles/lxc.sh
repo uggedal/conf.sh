c44c9026fc5c70ecf24a750ae931a8fe0246790a
Eivind Uggedal <eivind@uggedal.com>
2014-03-26 11:28:30 +0000

    lxc: create containers

--- roles/lxc.sh
+++ roles/lxc.sh
@@ -1,7 +1,5 @@
 _lxc_create() {
   local container=$1
-  local name=${container%%:*}
-  local release=${container##*:}
   local cmd
 
   lxc-ls | grep -q ^$name\$ && return
@@ -9,14 +7,34 @@ _lxc_create() {
   cmd="lxc-create -n $name -B $_lxc_fs -t alpine"
   cmd="$cmd -- --repository $_lxc_mirror/$release/main"
 
-  progress wrap lxc $name "$cmd >/dev/null"
+  progress wrap 'lxc create' $name "$cmd >/dev/null"
+}
+
+_lxc_containers() {
+  local ip_prefix=${_network_bridge_address%.*}
+  local ip_suffix=10
+  local name release rootfs
+
+  for container in $_lxc_containers; do
+    name=${container%%:*}
+    release=${container##*:}
+    ip_suffix=$(($ip_suffix + 1))
+    rootfs=/var/lib/lxc/$name/rootfs
+
+    _lxc_create $name $release || return 1
+
+    export _lxc_container_address=${ip_prefix}.${ip_suffix}
+    tmpl lxc.container.interfaces \
+      $rootfs/etc/network/interfaces || return 1
+
+    inode link $rootfs/etc/init.d/networking \
+      $rootfs/etc/runlevels/default/networking 777 || return 1
+  done
 }
 
 lxc_role() {
   pkg add lxc lxc-templates && \
     tmpl lxc.default.conf /etc/lxc/default.conf
 
-  for container in $_lxc_containers; do
-    _lxc_create $container
-  done
+  _lxc_containers
 }
