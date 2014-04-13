905504bb6f760da8321afe3ba698ff892c8364db
Eivind Uggedal <eivind@uggedal.com>
2014-04-13 13:03:43 +0000

    docker: enable btrfs support

---
+++ handlers/docker.sh
@@ -0,0 +1,3 @@
+docker_handler() {
+  daemon restart docker
+}
--- roles/docker.sh
+++ roles/docker.sh
@@ -1,5 +1,6 @@
 docker_role() {
   pkg add docker &&
+    tmpl confd.docker /etc/conf.d/docker docker
     daemon enable docker && \
     daemon start docker
 }
