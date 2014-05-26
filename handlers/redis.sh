f46ce5739975a262c0e4999d8f8c044d393d2ee3
Eivind Uggedal <eivind@uggedal.com>
2014-05-26 12:05:19 +0000

    redis: optimistic malloc

--- handlers/redis.sh
+++ handlers/redis.sh
@@ -1,3 +1,8 @@
 redis_handler() {
-  daemon restart redis
+  local kind=$1
+  local state=$2
+  local file=$3
+
+  [ "$file" = /etc/redis.conf ] && daemon restart redis
+  [ "$file" = /etc/sysctl.d/redis.conf ] && sysctl -q -p $file
 }
--- roles/redis.sh
+++ roles/redis.sh
@@ -1,6 +1,7 @@
 redis_role() {
   pkg add redis &&
     tmpl redis.conf /etc/redis.conf redis &&
+    tmpl sysctl.d.redis.conf /etc/sysctl.d/redis.conf redis &&
     daemon enable redis &&
     daemon start redis
 }
