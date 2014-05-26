e00f689b3d751beff003272677ed3b52725405d0
Eivind Uggedal <eivind@uggedal.com>
2014-05-26 09:03:03 +0000

    redis: conf

---
+++ handlers/redis.sh
@@ -0,0 +1,3 @@
+redis_handler() {
+  daemon restart redis
+}
--- roles/redis.sh
+++ roles/redis.sh
@@ -1,5 +1,6 @@
 redis_role() {
   pkg add redis &&
+    tmpl redis.conf /etc/redis.conf redis &&
     daemon enable redis &&
     daemon start redis
 }
