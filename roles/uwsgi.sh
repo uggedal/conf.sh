122a8b3b4633845ec4a41c9b5c7cb1b3151a5566
Eivind Uggedal <eivind@uggedal.com>
2014-05-27 17:41:41 +0000

    uwsgi: logging

--- roles/uwsgi.sh
+++ roles/uwsgi.sh
@@ -1,5 +1,6 @@
 uwsgi_role() {
   pkg add uwsgi uwsgi-python &&
+    inode dir /var/log/uwsgi 755 root &&
     tmpl conf.d.uwsgi /etc/conf.d/uwsgi uwsgi &&
     daemon enable uwsgi &&
     daemon start uwsgi
