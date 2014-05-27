e9aac36ff914dfcf53a4582b95b0d062b291e9cf
Eivind Uggedal <eivind@uggedal.com>
2014-05-27 17:35:03 +0000

    uwsgi: enable and start

--- roles/uwsgi.sh
+++ roles/uwsgi.sh
@@ -1,4 +1,6 @@
 uwsgi_role() {
   pkg add uwsgi uwsgi-python &&
-    tmpl conf.d.uwsgi /etc/conf.d/uwsgi uwsgi
+    tmpl conf.d.uwsgi /etc/conf.d/uwsgi uwsgi &&
+    daemon enable uwsgi &&
+    daemon start uwsgi
 }
