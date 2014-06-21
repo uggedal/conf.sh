f7ee6a7eb3efce0382b43c3c4822c1b60dc6c9b2
Eivind Uggedal <eivind@uggedal.com>
2014-06-21 07:56:38 +0000

    uwsgi: vassals config

--- roles/uwsgi.sh
+++ roles/uwsgi.sh
@@ -1,7 +1,36 @@
+_uwsgi_var() {
+  eval printf '%s' \$_uwsgi_${1}_${2}
+}
+
+_uwsgi_export() {
+  eval export _uwsgi_${2}="\$_uwsgi_${1}_${2}"
+}
+
+_uwsgi_vassals() {
+  local fqdn conf
+  for i in $(seq 1 9); do
+    fqdn=$(_uwsgi_var $i fqdn)
+    [ -z "$fqdn" ] && return
+
+    conf=/etc/uwsgi.d/${fqdn}.ini
+
+    inode file $conf 644 || return 1
+
+    for v in chdir module django processes idle; do
+      _uwsgi_export $i $v
+    done
+
+    tmpl vassal.conf $conf
+  done
+}
+
+
 uwsgi_role() {
   pkg add uwsgi uwsgi-python &&
-    inode dir /var/log/uwsgi 755 root &&
+    inode dir /etc/uwsgi.d 755 root &&
+    inode dir /var/log/uwsgi 755 nginx &&
     tmpl conf.d.uwsgi /etc/conf.d/uwsgi uwsgi &&
     daemon enable uwsgi &&
-    daemon start uwsgi
+    daemon start uwsgi &&
+    _uwsgi_vassals
 }
