eadde0cc133cf39140937ff22364fdac85004e40
Eivind Uggedal <eivind@uggedal.com>
2014-07-30 13:51:32 +0000

    cgit: logo

--- roles/cgit.sh
+++ roles/cgit.sh
@@ -1,5 +1,9 @@
 cgit_role() {
+  local logo_src=$(dirname $0)/files/git.uggedal.com.png
+  local logo_dest=/usr/share/webapps/cgit/$(basename $logo_src)
   pkg add cgit py-pygments
+
+  [ -f $logo_dest ] || cp $logo_src $logo_dest
 
   tmpl cgitrc /etc/cgitrc
 }
