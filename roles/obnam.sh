1c6a8e26573804b180811cb7eb3d7d2c4e66f025
Eivind Uggedal <eivind@uggedal.com>
2014-07-31 13:53:15 +0000

    obnam: backup and retention via cron

--- roles/obnam.sh
+++ roles/obnam.sh
@@ -1,3 +1,13 @@
 obnam_role() {
-  pkg add obnam
+  local conf=/etc/obnam.conf
+  local hourly=/etc/periodic/hourly/obnam
+  local daily=/etc/periodic/daily/obnam
+
+  pkg add obnam &&
+    inode file $conf 640 &&
+    inode file $hourly 740 &&
+    inode file $daily 740 &&
+    tmpl obnam.conf $conf &&
+    tmpl periodic.hourly.obnam $hourly &&
+    tmpl periodic.daily.obnam $daily
 }
