f3bae17c13e01c2d88e71e7c3cfd8233f86d00f0
Eivind Uggedal <eivind@uggedal.com>
2014-08-05 07:11:04 +0000

    cgit: use file

--- roles/cgit.sh
+++ roles/cgit.sh
@@ -1,20 +1,16 @@
 cgit_role() {
-  local logo_src=$(dirname $0)/files/git.uggedal.com.png
-  local logo_dest=/usr/share/webapps/cgit/$(basename $logo_src)
-  local highlight_src=$(dirname $0)/files/cgit.highlight.filter.py
-  local highlight_dest=/usr/lib/cgit/filters/highlight.py
-  local md_src=$(dirname $0)/files/cgit.md.filter.py
   local md_dest=/usr/lib/cgit/filters/md.py
+  local highlight_dest=/usr/lib/cgit/filters/highlight.py
 
   pkg add cgit py-pygments py-hoedown
 
-  [ -f $md_dest ] || cp $md_src $md_dest
+  file cgit.md.filter.py $md_dest
   inode file $md_dest 755
 
-  [ -f $highlight_dest ] || cp $highlight_src $highlight_dest
+  file cgit.highlight.filter.py $highlight_dest
   inode file $highlight_dest 755
 
-  [ -f $logo_dest ] || cp $logo_src $logo_dest
+  file git.uggedal.com.png /usr/share/webapps/cgit/git.uggedal.com.png
 
   tmpl cgitrc /etc/cgitrc
 }
