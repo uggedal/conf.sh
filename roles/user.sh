c136ee54cba57ca221d7e7f142c057307310a7e3
Eivind Uggedal <eivind@uggedal.com>
2014-08-14 13:22:11 +0000

    user: no need to unlock on pam

--- roles/user.sh
+++ roles/user.sh
@@ -1,7 +1,6 @@
 user_role() {
   pkg add bash git &&
     usr add -u $_user_name -g $_user_group -s /bin/bash &&
-    usr unlock $_user_name &&
     usr groups $_user_name "$_user_groups" &&
     usr sshkey $_user_name "$_user_sshkey" &&
     usr dotfiles $_user_name
