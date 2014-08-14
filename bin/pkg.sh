5a58bdb345416ce7557c166b179c29e568bd80cb
Eivind Uggedal <eivind@uggedal.com>
2014-08-14 10:20:42 +0000

    pkg: xbps support

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,39 +1,15 @@
-_pkg_world_cache=
-
 _pkg_installed() {
-  [ -n "$_pkg_world_cache" ] || _pkg_world_cache="$(cat /etc/apk/world)"
-
-  for p in $_pkg_world_cache; do
-    [ "$p" = "$1" ] && return
-  done
-  return 1
+  xbps-query $1 >/dev/null
 }
 
 _pkg_add() {
-  local err rc variants
+  local p
 
   for p do
-    variants=$p
-    [ -z "$_pkg_doc" ] || variants="$variants $p-doc"
-    [ -z "$_pkg_bash_completion" ] || variants="$variants $p-bash-completion"
+    _pkg_installed $p && continue
 
-    for v in $variants; do
-      _pkg_installed $v && continue
-
-      [ $v = $p ] || [ -n "$(apk info --quiet --size $v)" ] || continue
-
-      progress wrap 'pkg add' $v "apk add --quiet $v" || return 1
-    done
+    progress wrap 'pkg add' $p "xbps-install -y $p >/dev/null" || return 1
   done
-}
-
-_pkg_sync() {
-  progress wrap pkg sync 'apk update --quiet'
-}
-
-# TODO: apk update returns 0 on wget error
-_pkg_upgrade() {
-  progress wrap pkg upgrade 'apk upgrade --quiet'
 }
 
 pkg() {
