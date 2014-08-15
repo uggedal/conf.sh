9dc825d60402f4ee76b014ba60b503b171208592
Eivind Uggedal <eivind@uggedal.com>
2014-08-15 06:15:18 +0000

    sudo: do not break #includedir directive

--- roles/sudo.sh
+++ roles/sudo.sh
@@ -1,10 +1,7 @@
 sudo_role() {
-  local conf=/etc/sudoers
-  local wheel=/etc/sudoers.d/wheel
+  local f=/etc/sudoers.d/wheel
 
   pkg add sudo &&
-    inode file $conf 440 root &&
-    tmpl sudoers $conf &&
-    inode file $wheel 440 root &&
-    tmpl sudoers.d.wheel $wheel
+    inode file $f 440 root &&
+    tmpl sudoers.d.wheel $f
 }
