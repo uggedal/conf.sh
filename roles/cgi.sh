eac69bc6abf0d5509988fc492e71a18d17ebf201
Eivind Uggedal <eivind@uggedal.com>
2014-08-19 08:05:54 +0000

    cgi: adjustment for runit

--- roles/cgi.sh
+++ roles/cgi.sh
@@ -2,8 +2,9 @@ cgi_role() {
   local initd=/etc/init.d/spawn-fcgi
 
   pkg add fcgiwrap spawn-fcgi &&
-    inode link $initd $initd.fcgiwrap 755 &&
-    tmpl conf.d.spawn-fcgi /etc/conf.d/spawn-fcgi.fcgiwrap &&
-    daemon enable spawn-fcgi.fcgiwrap &&
-    daemon start spawn-fcgi.fcgiwrap
+    inode dir /etc/sv/spawn-fcgi &&
+    inode file /etc/sv/spawn-fcgi/run 755 &&
+    tmpl sv.spawn-fcgi.run /etc/sv/spawn-fcgi/run &&
+    inode link /run/runit/supervise.spawn-fcgi /etc/sv/spawn-fcgi/supervise &&
+    daemon enable spawn-fcgi
 }
