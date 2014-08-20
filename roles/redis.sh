e4ee1da1a7895b0cd3d7fc2b9738532f7fc7cdc6
Eivind Uggedal <eivind@uggedal.com>
2014-08-20 16:15:36 +0000

    redis: logrotate

--- roles/redis.sh
+++ roles/redis.sh
@@ -1,6 +1,7 @@
 redis_role() {
   pkg add redis &&
     inode dir /var/log/redis 750 redis &&
+    tmpl logrotate.d.redis /etc/logrotate.d/redis &&
     tmpl redis.redis.conf /etc/redis/redis.conf redis &&
     inode dir /etc/sysctl.d 755 &&
     tmpl sysctl.d.redis.conf /etc/sysctl.d/redis.conf redis &&
