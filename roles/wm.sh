84a061ad761bfb617e068fa5f8531e9c2cb23f2f
Eivind Uggedal <eivind@uggedal.com>
2014-08-29 09:48:47 +0200

    wm: new role

---
+++ roles/wm.sh
@@ -0,0 +1,12 @@
+wm_role() {
+  pkg add xset \
+    xrdb \
+    setxkbmap \
+    xsetroot \
+    unclutter \
+    autocutsel \
+    herbstluftwm \
+    dzen2 \
+    dejavu-fonts-ttf \
+    rxvt-unicode
+}
