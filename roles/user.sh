166e759677ffa318674971fb99a390ebf23fc160
Eivind Uggedal <eivind@uggedal.com>
2014-11-18 09:03:21 +0000

    user: preserve autologin

--- roles/user.sh
+++ roles/user.sh
@@ -1,10 +1,15 @@
 user_role() {
+  local preserve_d=/etc/xbps/preserve.d
+
   pkg add bash git &&
     usr add -u $_user_name -g $_user_group -s /bin/bash &&
     usr groups $_user_name $_user_groups &&
     usr sshkey $_user_name "$_user_sshkey" &&
     usr dotfiles $_user_name
 
-  [ -z "$_user_autologin" ] |
-    tmpl -s /etc/sv/agetty-tty1/run
+  [ -z "$_user_autologin" ] || {
+    inode dir $preserve_d 755 &&
+      tmpl -s $preserve_d/runit-void.conf &&
+      tmpl -s /etc/sv/agetty-tty1/run
+  }
 }
