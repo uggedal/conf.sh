a699f9150aba8bcd65d409db9051172fbf1aae11
Eivind Uggedal <eivind@uggedal.com>
2014-12-03 23:13:46 +0000

    user: default to zsh

--- roles/user.sh
+++ roles/user.sh
@@ -1,8 +1,10 @@
 user_role() {
   local preserve_d=/etc/xbps/preserve.d
 
+  pkg add zsh
+
   pkg add bash git &&
-    usr add -u $_user_name -g $_user_group -s /bin/bash &&
+    usr add -u $_user_name -g $_user_group -s /bin/zsh &&
     usr groups $_user_name $_user_groups &&
     usr sshkey $_user_name "$_user_sshkey" &&
     usr dotfiles $_user_name
