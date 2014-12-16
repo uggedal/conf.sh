6ed06aac3ddd6ac56d5f019940aa6b61fc6c49fd
Eivind Uggedal <eivind@uggedal.com>
2014-12-16 13:09:31 +0000

    nftables: simplify flush/delete handler

--- handlers/nftables.sh
+++ handlers/nftables.sh
@@ -5,10 +5,6 @@ nftables_handler() {
 
   nft list tables inet | while read _ t; do
     nft flush table inet $t
-    nft list table inet $t | awk "/$chain_re/ { print \$2 }" | while read c; do
-      nft flush chain inet $t $c
-      nft delete chain inet $t $c
-    done
     nft delete table inet $t
   done
 
