9bd20294bc25d8106dbdafaaf073887fe1b0a67a
Eivind Uggedal <eivind@uggedal.com>
2014-12-18 07:51:44 +0000

    user: zsh-syntax-highlighting

--- roles/user.sh
+++ roles/user.sh
@@ -1,9 +1,9 @@
 user_role() {
   local preserve_d=/etc/xbps/preserve.d
 
-  pkg add zsh
-
-  pkg add bash git &&
+  pkg add zsh \
+    zsh-syntax-highlighting \
+    git &&
     usr add -u $_user_name -g $_user_group -s /bin/zsh &&
     usr groups $_user_name $_user_groups &&
     usr sshkey $_user_name "$_user_sshkey" &&
