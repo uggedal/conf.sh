7690f8fdffa15bab7e20c4a4091c308b8bf0e3a6
Eivind Uggedal <eivind@uggedal.com>
2015-01-06 09:45:47 +0000

    lxc: create containers

--- roles/lxc.sh
+++ roles/lxc.sh
@@ -1,4 +1,20 @@
+_lxc_containers() {
+  local i name template
+  for i in $(seq 1 9); do
+    name=$(var get lxc $i name)
+    template=$(var get lxc $i template)
+    [ -z "$name" ] && return
+    [ -z "$template" ] && template=void
+
+    lxc-ls | grep -q ^$name\$ && return
+
+    progress wrap 'lxc create' $name \
+      "lxc-create -n $name -t $template >/dev/null"
+  done
+}
+
 lxc_role() {
   pkg add lxc &&
-    tmpl -s /etc/lxc/default.conf
+    tmpl -s /etc/lxc/default.conf &&
+    _lxc_containers
 }
