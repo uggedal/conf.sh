3b267d25e117255329d0dbe1673bef29538f0cb6
Eivind Uggedal <eivind@uggedal.com>
2015-01-20 13:30:41 +0000

    miniflux: feed fetch from cron

--- roles/miniflux.sh
+++ roles/miniflux.sh
@@ -1,4 +1,10 @@
 miniflux_role() {
+  local db=/var/lib/miniflux/db.sqlite
+  local cron=/var/spool/cron/miniflux
+
   pkg add miniflux &&
-    usr groups nginx miniflux
+    usr groups nginx miniflux &&
+    inode file $db 660 miniflux &&
+    inode file $cron 600 &&
+    tmpl -s $cron
 }
