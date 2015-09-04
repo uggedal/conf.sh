bc6f531ef11fd964ac3f8c84c9a600d563807460
Eivind Uggedal <eivind@uggedal.com>
2015-09-04 10:43:01 +0000

    usr: addgroup/adduser support

--- bin/usr.sh
+++ bin/usr.sh
@@ -3,10 +3,16 @@ _usr_field() {
 }
 
 _usr_add() {
-  local opt user group shell
+  local opt user group shell gcmd ucmd
 
-  local gcmd=groupadd
-  local ucmd=useradd
+  local gbin=groupadd
+  local ubin=useradd
+
+  command -v $gbin >/dev/null || gbin=addgroup
+  command -v $ubin >/dev/null || {
+    ubin=adduser
+    ucmd="-D"
+  }
 
   while getopts "u:g:s:h:S" opt; do
     case $opt in
@@ -15,18 +21,31 @@ _usr_add() {
         ;;
       g)
         group=$OPTARG
-        ucmd="$ucmd -g $OPTARG"
+        if [ "$ubin" = useradd ]; then
+          ucmd="$ucmd -g $OPTARG"
+        else
+          ucmd="$ucmd -G $OPTARG"
+        fi
         ;;
       s)
         shell=$OPTARG
         ucmd="$ucmd -s $OPTARG"
         ;;
       h)
-        ucmd="$ucmd -M -d $OPTARG"
+        if [ "$ubin" = useradd ]; then
+          ucmd="$ucmd -M -d $OPTARG"
+        else
+          ucmd="$ucmd -H -h $OPTARG"
+        fi
         ;;
       S)
-        gcmd="$gcmd -r"
-        ucmd="$ucmd -r"
+        if [ "$ubin" = useradd ]; then
+          gcmd="$gcmd -r"
+          ucmd="$ucmd -r"
+        else
+          gcmd="$gcmd -S"
+          ucmd="$ucmd -S"
+        fi
         ;;
     esac
   done
@@ -34,13 +53,20 @@ _usr_add() {
   [ -n "$user" -a -n "$group" ] || return 1
 
   _usr_field group 1 $group | grep -q "^$group\$" ||
-    progress wrap 'usr group' $user "$gcmd $group"
+    progress wrap 'usr group' $user "$gbin $gcmd $group"
 
   _usr_field passwd 1 $user | grep -q "^$user\$" ||
-    progress wrap 'usr add' $user "$ucmd $user"
+    progress wrap 'usr add' $user "$ubin $ucmd $user"
+
+  local chsh
+  if command -v chsh >/dev/null; then
+    chsh="chsh -s $shell $user"
+  else
+    chsh="sed -i '/$user:/s|:[^:]*$|:$shell|' /etc/passwd"
+  fi
 
   [ "$(_usr_field passwd 7 $user)" = "$shell" ] ||
-    progress wrap 'usr shell' "$user $shell" "chsh -s $shell $user"
+    progress wrap 'usr shell' "$user $shell" "$chsh"
 }
 
 _usr_groups() {
@@ -49,14 +75,22 @@ _usr_groups() {
   local groups
   groups="$@"
   local ok=1
+  local cmd
 
   for g in $groups; do
-    id -nG $user | tr ' ' '\n' | fgrep -q $g || ok=0
+    ! id -nG $user | tr ' ' '\n' | fgrep -q $g || continue
+
+    if command -v usermod >/dev/null; then
+      ok=0
+    elif command -v addgroup >/dev/null; then
+      cmd="${cmd}addgroup $user $g && "
+    fi
   done
 
   local csv=$(printf "$groups" | tr ' ' ',')
 
   [ "$ok" -eq 1 ] || progress wrap 'usr groups' $user "usermod -aG $csv $user"
+  [ -z "$cmd" ] || progress wrap 'usr groups' $user "${cmd}true"
 }
 
 _usr_sshkey() {
