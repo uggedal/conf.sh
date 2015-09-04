b9e9a540f516d7cf3d964357a7c17eadf5ab092c
Eivind Uggedal <eivind@uggedal.com>
2015-09-04 10:43:12 +0000

    pkg: apk support

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,5 +1,21 @@
+_pkg_world_cache=
+
 _pkg_installed() {
-  xbps-query $1 >/dev/null
+  local p
+
+  case $system in
+    void)
+      xbps-query $1 >/dev/null
+      ;;
+    alpine)
+      [ -n "$_pkg_world_cache" ] || _pkg_world_cache="$(cat /etc/apk/world)"
+
+      for p in $_pkg_world_cache; do
+        [ "$p" = "$1" ] && return
+      done
+      return 1
+      ;;
+  esac
 }
 
 _pkg_add() {
@@ -8,7 +24,14 @@ _pkg_add() {
   for p do
     _pkg_installed $p && continue
 
-    progress wrap 'pkg add' $p "xbps-install -y $p >/dev/null" || return 1
+    case $system in
+      void)
+        progress wrap 'pkg add' $p "xbps-install -y $p >/dev/null" || return 1
+        ;;
+      alpine)
+        progress wrap 'pkg add' $p "apk add --quiet $p" || return 1
+        ;;
+    esac
   done
 }
 
